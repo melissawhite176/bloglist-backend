84a90d2e34ca635755a6f8f373be83a8dbcc60b3
Melissa White <melissawhite176@gmail.com>
2020-07-16 12:18:29 -0700

    create mongo.js file with mongoose require to create new blogs to database and finds blogs stored in database - bloglist backend

---
+++ mongo.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack-bloglist:${password}@cluster0-klm2a.mongodb.net/bloglist-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+//GENERATE NEW BLOG
+//-----------------
+
+const blog = new Blog({
+  title: 'Chris Crafts Blog',
+  author: 'Chris',
+  url: 'https://www.mongoosejs.com',
+  likes: 150
+})
+
+blog.save().then(result => {
+  console.log('blog saved!')
+  mongoose.connection.close()
+})
+
+//PRINTS BLOGS STORED IN DATABASE 
+//----------------------------
+
+// Blog.find({ author: 'Marie' }).then(result => {
+//   result.forEach(blog => {
+//     console.log(blog)
+//   })
+//   mongoose.connection.close()
+// })
