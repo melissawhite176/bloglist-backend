e14373a23fc4de4533023173c6295d359482e20c
Melissa White <melissawhite176@gmail.com>
2020-07-28 12:27:30 -0700

    create utils/middleware.js for custom middleware

---
+++ utils/middleware.js
@@ -0,0 +1,45 @@
+//custom middleware
+const logger = require('./logger')
+
+//----------REQUEST LOGGER-----------
+//middleware that prints information about every request that is
+//sent to the server (middleware receives these three parameters)
+const requestLogger = (request, response, next) => {
+  logger.info('Method:', request.method)
+  logger.info('Path: ', request.path)
+  logger.info('Body: ', request.body)
+  logger.info('---')
+  next()
+}
+
+//----------UNKNOWN ENDPOINT------------
+//middleware used for catching requests made to non-existent routes
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+//-----------ERROR HANDLER--------------
+//check if the error is a CastError exception (ex: invalid object id for Mongo)
+//in all other error situations,
+//the middleware will pass the error forward to the default Express error handler
+const errorHandler = (error, request, response, next) => {
+  logger.info('error.name:', error.name)
+  logger.info('error message:', error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400)
+      .json({ errorname: error.name, error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400)
+      .json({ errorname: error.name, error: error.message })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
+
